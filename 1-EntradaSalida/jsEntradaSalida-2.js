514656fe977dcd7a4ebc45b6d00f7225734fbfa1
MartinX123 <47385885+MartinX123@users.noreply.github.com>
2019-02-06 12:26:15 -0300

    Add files via upload

--- 1-EntradaSalida/jsEntradaSalida-2.js
+++ 1-EntradaSalida/jsEntradaSalida-2.js
@@ -1,7 +1,9 @@
 /*Debemos lograr tomar un nombre con 'prompt' 
 y luego mostrarlo por 'alert' al presionar el botón  'MOSTRAR'*/
-function Mostrar()
+function Mostar()
 {
-	
+var nombre;
+nombre=prompt("escriba su nombre");
+alert(nombre);
 }
 
--- 1-EntradaSalida/jsEntradaSalida-5.js
+++ 1-EntradaSalida/jsEntradaSalida-5.js
@@ -1,7 +1,14 @@
 /*Debemos lograr tomar nombre y edad por ID y mostrarlos concatenados 
 ej.: "Usted se llama José y tiene 66 años" 	*/
-function Mostrar()
+function Mostar()
 {	
-	
+	var nombre;
+    var edad;
+    var x="usted se llama";
+    var y="y tiene";
+    var d="años;"
+    nombre=document.getElementById("elNombre").value;
+    edad=document.getElementById("laEdad").value;
+    alert(x+""+nombre+""+y+""+edad+""+d);
 }
 
