fc23338c82e86d0fc0ada39b5d7814a4dead7abf
MartinX123 <47385885+MartinX123@users.noreply.github.com>
2019-02-14 12:19:50 -0300

    Add files via upload

--- 2-InstruccionIf/jsInstruccionIF (6).js
+++ 2-InstruccionIf/jsInstruccionIF (6).js
@@ -1,6 +1,19 @@
 function Mostrar()
 {
 //tomo la edad  
+var edad
+edad = parseInt(document.getElementById("edad").value);
+
+if(edad < 13)
+    {alert("Usted es un niño");}
+
+else if(edad <=17)
+{alert("Usted es adolescente");}
+
+else if(edad <=70)
+    {alert("Usted es un adulto");}
+    else
+    {alert("Usted es un anciano")}
 
 
 
