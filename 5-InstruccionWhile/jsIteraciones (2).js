2d0b00d1be5e4c2ab2c9ca87e0a917dc2fdbe6f0
MartinX123 <47385885+MartinX123@users.noreply.github.com>
2019-02-27 12:25:43 -0300

    Add files via upload

--- 5-InstruccionWhile/jsIteraciones (2).js
+++ 5-InstruccionWhile/jsIteraciones (2).js
@@ -1,6 +1,11 @@
 function Mostrar()
 {
-	alert('iteración while');
+	var contador;
+	contador=11
 
+	while (contador>1){
+		contador=contador-1
+		alert(contador)
+	}
 
 }//FIN DE LA FUNCIÓN
--- 5-InstruccionWhile/jsIteraciones (4).js
+++ 5-InstruccionWhile/jsIteraciones (4).js
@@ -2,6 +2,9 @@ function Mostrar()
 {
 
 	var numero = prompt("ingrese un número entre 0 y 10.");
+	while(numero=NaN){
+		alert("no es un ")
+	}
 
 
 }//FIN DE LA FUNCIÓN
